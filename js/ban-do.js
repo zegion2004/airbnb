94c62932fb24e0c0a2b5813d294bac47a9617f83
zegion2004 <168709500+zegion2004@users.noreply.github.com>
2025-01-04 00:06:47 +0700

    them hien thi ban do

---
+++ js/ban-do.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const roomList = document.querySelector(".room-list");
+    const fixedButton = document.querySelector(".fixed-button");
+  
+    // Tạo Intersection Observer
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio > 0.3) {
+            // Hiển thị nút khi 50% room-list còn trong màn hình
+            fixedButton.classList.remove("hidden");
+          } else {
+            // Ẩn nút khi ít hơn 50% room-list còn trong màn hình
+            fixedButton.classList.add("hidden");
+          }
+        });
+      },
+      {
+        threshold: 0.3, // Kích hoạt khi 50% phần tử room-list xuất hiện
+      }
+    );
+  
+    if (roomList) {
+      observer.observe(roomList); // Theo dõi room-list
+    }
+  });
+  
